fb9b6f63bf9e3243b0b465ff30aca63ac9929af2
caleron <caleron@users.noreply.github.com>
2017-06-10 13:09:29 +0200

    added links to menu

--- src/components/DrawerMenu.vue
+++ src/components/DrawerMenu.vue
@@ -9,18 +9,20 @@
         </v-flex>
       </v-layout>
       <v-divider dark v-else-if="item.divider" class="my-4" :key="i"></v-divider>
-      <v-list-item :key="i" v-else>
-        <v-list-tile>
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>
-              {{ item.text }}
-            </v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list-item>
+      <router-link :to="item.link" v-else>
+        <v-list-item :key="i">
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>
+                {{ item.text }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list-item>
+      </router-link>
     </template>
   </v-list>
 </template>
@@ -32,17 +34,24 @@
     data () {
       return {
         items: [
-          {icon: 'music_note', text: 'Library'},
+          {icon: 'music_note', text: 'Library', link: '/'},
           {divider: true},
           {heading: 'Playlists'},
-          {icon: 'queue_music', text: 'Queue'},
+          {icon: 'queue_music', text: 'Queue', link: '/queue'},
           {divider: true},
           {heading: 'System'},
-          {icon: 'lightbulb_outline', text: 'Light'},
-          {icon: 'power_settings_new', text: 'Power'},
-          {icon: 'settings', text: 'Settings'}
+          {icon: 'lightbulb_outline', text: 'Light', link: '/light'},
+          {icon: 'power_settings_new', text: 'Power', link: '/power'},
+          {icon: 'settings', text: 'Settings', link: '/settings'}
         ]
       }
     }
   }
 </script>
+
+<style scoped>
+  .router-link-exact-active > li {
+    background: #8c9eff;
+  }
+
+</style>
